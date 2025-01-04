82ca8d1457f1955069c8d922aba0998eccd6d06f
Miro Sieber <160486278+Msfjel383@users.noreply.github.com>
2025-01-04 15:51:32 +0100

    Modified to be compatibel with ESP32 adc

--- examples/QTRAExample/QTRAExample.ino
+++ examples/QTRAExample/QTRAExample.ino
@@ -28,15 +28,16 @@
 
 QTRSensors qtr;
 
-const uint8_t SensorCount = 6;
+const uint8_t SensorCount = 5;
 uint16_t sensorValues[SensorCount];
 
 void setup()
 {
   // configure the sensors
   qtr.setTypeAnalog();
-  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5}, SensorCount);
-  qtr.setEmitterPin(2);
+  qtr.setSensorPins((const uint8_t[]){1, 2, 3, 4, 5, 6, 7, 8}, SensorCount);
+  pinMode(47, OUTPUT);
+  digitalWrite(47,HIGH);
 
   delay(500);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -46,7 +47,7 @@ void setup()
   // 0.1 ms per sensor * 4 samples per sensor read (default) * 6 sensors
   // * 10 reads per calibrate() call = ~24 ms per calibrate() call.
   // Call calibrate() 400 times to make calibration take about 10 seconds.
-  for (uint16_t i = 0; i < 400; i++)
+  for (uint16_t i = 0; i < 800; i++)
   {
     qtr.calibrate();
   }
@@ -76,17 +77,39 @@ void loop()
 {
   // read calibrated sensor values and obtain a measure of the line position
   // from 0 to 5000 (for a white line, use readLineWhite() instead)
-  uint16_t position = qtr.readLineBlack(sensorValues);
-
-  // print the sensor values as numbers from 0 to 1000, where 0 means maximum
-  // reflectance and 1000 means minimum reflectance, followed by the line
-  // position
-  for (uint8_t i = 0; i < SensorCount; i++)
+  qtr.read(sensorValues);
+  /*
+  
+   for (uint8_t i = 0; i < SensorCount; i++)
   {
     Serial.print(sensorValues[i]);
     Serial.print('\t');
   }
-  Serial.println(position);
+  Serial.println();
+  */
+  //qtr.readLineBlack(sensorValues);
+  // print the sensor values as numbers from 0 to 1000, where 0 means maximum
+  // reflectance and 1000 means minimum reflectance, followed by the line
+  // position
+  uint16_t summ=0;
+  uint16_t awg=0;
+  for (uint8_t i = 0; i < SensorCount; i++){
+    //Werte hinter der linie abschneiden
+    uint16_t last = 0;
+    if(i>0){
+      last = sensorValues[i-1];
+    }
+    
+    if(last>sensorValues[i]){
+      sensorValues[i]=last;
+    }
+    summ += sensorValues[i];
+    Serial.print(sensorValues[i]);
+    Serial.print('\t');
+  }
+  awg= summ/SensorCount;
 
-  delay(250);
+  Serial.println(awg);
+
+  delay(10);
 }
